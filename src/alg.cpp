00f87035716b6c3b51596203613453f11c785722
dariyadmitrievnad-lgtm <dariyadmitrievnad@gmail.com>
2026-04-09 00:11:05 +0300

    Демидова Д.Д 25-ИСТ-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,44 @@
-// Copyright 2025 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+#define SIZE 100
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
-}
+template <typename T> class TStack {
+ private:
+  T stack[SIZE];
+  int topIn;
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
-}
+ public:
+  TStack() : topIn(-1) {}
+
+  int push(T value) {
+    if (topIn >= SIZE - 1) {
+      throw std::runtime_error("Stack overflow!");
+    }
+    stack[++topIn] = value;
+    return 0;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      throw std::runtime_error("Stack is empty!!");
+    }
+    return stack[topIn--];
+  }
+
+  T top() const {
+    if (isEmpty()) {
+      throw std::runtime_error("Stack is empty!");
+    }
+    return stack[topIn];
+  }
+
+  bool isEmpty() const { return topIn == -1; }
+
+  bool isFull() const { 4 return topIn == SIZE - 1; }
+  bool isFull() const { return topInd == SIZE - 1; }
+
+  int getSize() const { return topInd + 1; }
+};
+
+#endif // INCLUDE_TSTACK_H_
